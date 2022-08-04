964559111d3bdd40951f452cbcfc27f4e2c76b04
Nick Coutts <Nickcoutts99@gmail.com>
2022-08-04 05:38:52 -0400

    Added very basic fields and files

--- RecipeBlog/src/main/java/RecipeBlog/controller/WebController.java
+++ RecipeBlog/src/main/java/RecipeBlog/controller/WebController.java
@@ -1,4 +1,14 @@
 package RecipeBlog.controller;
 
+import RecipeBlog.model.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebController {
+
+    @GetMapping("/Recipes")
+    public String getRecipes(){
+        return "This will be a list of recipes";
+    }
 }
---
+++ RecipeBlog/src/main/java/RecipeBlog/model/Ingredient.java
@@ -0,0 +1,28 @@
+package RecipeBlog.model;
+
+public class Ingredient {
+
+    private String name;
+    private String amount;
+
+    public Ingredient(String name, String amount){
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
